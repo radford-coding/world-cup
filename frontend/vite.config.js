8a33de662df3edd5a6280ac8736c92db7b078695
Matt Radford <matt@Grogu.local>
2026-06-14 21:36:28 -0700

    update paths for correctness

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/world-cup/',
+  base: '/world-cup/frontend/dist/',
   plugins: [react()],
 })
