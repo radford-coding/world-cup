c54743f7cc97a1b184aaeca64defd0e5de40933c
Matt Radford <matt@Grogu.local>
2026-06-14 21:09:22 -0700

    rewrite for github pages deploy; frontend only

--- backend/league/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('games/', views.GamesView.as_view(), name='games'),
-    path('teams/', views.TeamsView.as_view(), name='teams'),
-    path('standings/', views.StandingsView.as_view(), name='standings'),
-]
